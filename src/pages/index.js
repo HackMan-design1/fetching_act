fa3651426fe54b5689ac0fdcedb3617bedcb25d6
HackMan-design1 <noorajohn54@gmail.com>
2024-10-10 12:48:20 +0800

    api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import {
-  
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-   
-  
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
 } from "@/components/ui/table"
 
 const queryClient = new QueryClient()
@@ -27,40 +29,45 @@ function Example() {
     queryFn: () =>
       fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population').then((res) =>
         res.json(),
+    
+    
+  
       ),
+      
   })
 
   if (isLoading) return 'Loading...'
 
   if (error) return 'An error has occurred: ' + error.message
-  console.log(data.data)
-  const user_data = data.data
+
+  const user_data=data.data
+  
 
   return (
     <Table>
-  <TableCaption>A list of your recent invoices.</TableCaption>
-  <TableHeader>
-    <TableRow>
-      <TableHead className="w-[100px]">Nation</TableHead>
-      <TableHead>Year</TableHead>
-      <TableHead>Population</TableHead>
-      <TableHead className="text-right">Slug.Nation</TableHead>
-    </TableRow>
-  </TableHeader>
-  <TableBody>
-    <TableRow>
-      (user_data.map(data.Key))
-      <TableCell className="font-medium">(data.Nation)</TableCell>
-      <TableCell>(data.Nation)</TableCell>
-      <TableCell>(data.Population)</TableCell>
-      <TableCell className="text-right">(data.Nation)</TableCell>
-    </TableRow>
-  </TableBody>
-</Table>
-
-
-
-
-   
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">ID Nation</TableHead>
+          <TableHead className="text-right">Nation</TableHead>
+          <TableHead className="text-right">ID Year</TableHead>
+          <TableHead className="text-right">Year</TableHead>
+          <TableHead className="text-right">Population</TableHead>
+          <TableHead className="text-right">Slug Nation</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {user_data.map((data, key) => (
+          <TableRow key={key}>
+            <TableCell className="font-medium">{data['ID Nation']}</TableCell>
+            <TableCell className="text-right">{data.Nation}</TableCell>
+            <TableCell className="text-right">{data['ID Year']}</TableCell>
+            <TableCell className="text-right">{data.Year}</TableCell>
+            <TableCell className="text-right">{data.Population}</TableCell>
+            <TableCell className="text-right">{data['Slug Nation']}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+     
+      </Table>
   )
 }
